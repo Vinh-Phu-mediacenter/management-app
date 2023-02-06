9fb5dea89a5f6a1028120a299ff10b33d2358dfc
Vinh-Phu-mediacenter <phu.vinh@mediacenter.net.vn>
2023-02-06 15:25:01 +0700

    set up

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Home></Home>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from "./components/Home.vue";
+import "./assets/tailwind.css";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Home,
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <header class="bg-blue-500">demo</header>
+    <footer>{{ msg }}</footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home-app",
+  data() {
+    return {
+      msg: "asdgfsgfgsad",
+    };
+  },
+};
+</script>
